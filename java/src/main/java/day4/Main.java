37283424bf5740d5042d2526b355f64a8f32cee3
dmn1k <dominik.schlosser@gmail.com>
2019-12-04 21:49:31 +0100

    Refactoring

--- java/src/main/java/day4/Main.java
+++ java/src/main/java/day4/Main.java
@@ -1,73 +1,43 @@
 package day4;
 
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
         long passwordCount1 = IntStream.rangeClosed(136760, 595730)
-                .filter(Main::hasSameDigitsConsecutively)
+                .mapToObj(String::valueOf)
                 .filter(Main::digitsNeverDecrease)
+                .map(Main::getDigitFrequency)
+                .filter(freq -> freq.values().stream().anyMatch(count -> count >= 2))
                 .count();
 
         System.out.println("Part 1: " + passwordCount1);
 
         long passwordCount2 = IntStream.rangeClosed(136760, 595730)
-                .filter(Main::hasSameDigitsRepeatedExactlyTwice)
+                .mapToObj(String::valueOf)
                 .filter(Main::digitsNeverDecrease)
+                .map(Main::getDigitFrequency)
+                .filter(freq -> freq.containsValue(2L))
                 .count();
 
         System.out.println("Part 2: " + passwordCount2);
     }
 
-    private static boolean digitsNeverDecrease(int input) {
-        int digitCount = getDigitCount(input);
-        for (int i = 1; i < digitCount; i++) {
-            if (nthDigit(input, i) < nthDigit(input, i + 1)) {
-                return false;
-            }
-        }
+    private static boolean digitsNeverDecrease(String input) {
+        String sorted = Arrays.stream(input.split(""))
+                .sorted()
+                .collect(Collectors.joining());
 
-        return true;
+        return input.equals(sorted);
     }
 
-    private static int nthDigit(int input, int digit) {
-        return (input % ((int) Math.pow(10, digit))) / (int) Math.pow(10, digit - 1);
-    }
-
-    private static boolean hasSameDigitsConsecutively(int input) {
-        String inputAsStr = String.valueOf(input);
-        return inputAsStr.contains("11")
-                || inputAsStr.contains("22")
-                || inputAsStr.contains("33")
-                || inputAsStr.contains("44")
-                || inputAsStr.contains("55")
-                || inputAsStr.contains("66")
-                || inputAsStr.contains("77")
-                || inputAsStr.contains("88")
-                || inputAsStr.contains("99")
-                || inputAsStr.contains("00");
-    }
-
-    private static boolean hasSameDigitsRepeatedExactlyTwice(int input) {
-        int digitCount = getDigitCount(input);
-        for (int i = 1; i < digitCount; i++) {
-            int digitBeforeCurrent = i > 1 ? nthDigit(input, i - 1) : -1;
-            int currentDigit = nthDigit(input, i);
-            int nextDigit = nthDigit(input, i + 1);
-            int digitAfterNext = i < digitCount - 1 ? nthDigit(input, i + 2) : -1;
-
-            if (digitBeforeCurrent != currentDigit
-                    && currentDigit == nextDigit
-                    && nextDigit != digitAfterNext) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    private static int getDigitCount(int input) {
-        return (int) (Math.log10(input) + 1);
+    private static Map<String, Long> getDigitFrequency(String input) {
+        return Arrays.stream(input.split(""))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
     }
 }
